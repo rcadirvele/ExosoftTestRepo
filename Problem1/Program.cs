1eee7f77bf3691e4223910271ab5f42413d031fb
ram <rcadirvele@outlook.com>
2023-12-17 14:40:48 +0000

    check

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worldsdfd!");
-
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Worldsdfd!");
+    }
+}
