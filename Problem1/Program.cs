beff92bfef7662b0da48136a5262fa920b1d0e86
ram <rcadirvele@outlook.com>
2023-12-17 15:27:33 +0000

    Logic implemented for both problems

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,7 +1,44 @@
-﻿public class Program
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, Worldsdfd!");
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string filePath = Path.Combine(path, "palindromes.txt");
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                if (ContainsPalindrome(line))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("'palindromes.txt' file was not found.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+    }
+
+    static bool ContainsPalindrome(string fileLines)
+    {
+        string newFileLines = new string(fileLines
+            .Where(c => Char.IsLetterOrDigit(c))
+            .ToArray())
+            .ToLower();
+
+        return newFileLines.SequenceEqual(newFileLines.Reverse());
     }
 }
---
+++ Problem2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string filePath = Path.Combine(desktopPath, "numbers.txt");
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            string concatAllText = string.Join(" ", lines);
+
+            var matchedNumbers = Regex.Matches(concatAllText, @"\d+")
+                .Cast<Match>()
+                .Select(match => int.Parse(match.Value));
+
+            var topNumbers = matchedNumbers
+                .GroupBy(n => n)
+                .OrderByDescending(g => g.Count())
+                .Take(5)
+                .Select(g => g.Key);
+
+            Console.WriteLine("Five numbers that occurs most times:");
+
+            foreach (var number in topNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("The file 'numbers.txt' was not found!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+    }
+}
